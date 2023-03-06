bccf115d475b487c36ebdd78de4e1bb2e03c6333
Sachinda Liyanaarachchi <sachinda.yl@gmail.com>
2023-03-06 12:08:45 -0500

    create metric circle progress

---
+++ app/src/main/java/com/technomatesoftware/ergostats/components/Metric Circle.kt
@@ -0,0 +1,41 @@
+package com.technomatesoftware.ergostats.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MetricCircle() {
+
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            CircularProgressIndicator(
+                progress = 0.8f,
+                strokeWidth= 8.dp,
+                modifier = Modifier
+                    .width(100.dp)
+                    .height(100.dp),
+            )
+        }
+        Text("26.5", color = Color.White, fontWeight = FontWeight.SemiBold, fontSize = 20.sp)
+    }
+
+}
+
+@Preview
+@Composable
+fun MetricCirclePreview() {
+    MetricCircle()
+}

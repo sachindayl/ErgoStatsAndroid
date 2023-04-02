e353873192543b7b8c7708447b62b11d5f93c70b
Sachinda Liyanaarachchi <sachinda.yl@gmail.com>
2023-04-02 16:11:06 -0400

    rich list numbering fix

--- app/src/main/java/com/technomatesoftware/ergostats/views/richlist/RichListView.kt
+++ app/src/main/java/com/technomatesoftware/ergostats/views/richlist/RichListView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -26,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -81,8 +83,12 @@ fun RichListView(
                     },
                     supportingText = { Text(model.getFormattedBalance()) },
                     leadingContent = {
-                        Box(contentAlignment = Alignment.Center, modifier = Modifier.width(20.dp)) {
-                            Text("${index + 1}")
+                        Box(contentAlignment = Alignment.Center, modifier = Modifier.width(30.dp)) {
+                            Text(
+                                "${index + 1}",
+                                textAlign = TextAlign.Center,
+                                modifier = Modifier.fillMaxWidth()
+                            )
                         }
 
                     },
